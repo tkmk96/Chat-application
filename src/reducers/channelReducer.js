80119bfb9a7100cf91aaa332440da539fab0ee17
Lenka Janeckova <lenka.janeckova88@gmail.com>
2017-11-17 22:39:45 +0100

    Sorting messages by date

--- src/reducers/channelReducer.js
+++ src/reducers/channelReducer.js
@@ -14,6 +14,9 @@ export const activeChannel = (state = {channelId: null, messages: []}, action) =
     switch (action.type) {
         case SET_ACTIVE_CHANNEL:
             const {channelId, messages} = action.payload;
+            messages.sort((m1, m2) => {
+                return new Date(m1.createdAt).getTime() > new Date(m2.createdAt).getTime();
+            });
             return {channelId, messages};
         case CREATE_MESSAGE:
             return {channelId: action.payload.channelId, messages: [...state.messages, action.payload.message]};
