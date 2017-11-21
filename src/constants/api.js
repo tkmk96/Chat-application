3f56c16b322573e9bad3df1b8a940e73f612712f
tkmk96 <tkmk96@gmail.com>
2017-11-21 16:31:30 +0100

    Changed APP_ID

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const APP_ID = '8d324e0b-9060-45ee-9e85-ed0030220f31';
+export const APP_ID = '9b8b673a-99ab-41fa-8c12-681f659ef888';
 export const API_URL = 'https://pv247messaging.azurewebsites.net/api';
 export const API_CHANNEL = '/channels';
