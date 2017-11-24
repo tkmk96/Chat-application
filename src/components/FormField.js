e92ee520c01258b8b1f8a1ffa9638afb8cfd657a
tkmk96 <tkmk96@gmail.com>
2017-11-24 22:39:02 +0100

    Fixed displaying of error div in FormField component

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+function _renderError(touched, error){
+    if (touched && error){
+        return <div className='red-text'>{error}</div>;
+    }
+    return null;
+}
+
 export default ({label, type, input, style, placeholder, meta: {touched, error}}) => {
     return (
         <div className='input-field' style={style}>
             <input id={input.name} type={type} placeholder={placeholder} {...input}/>
             <label htmlFor={input.name}>{label}</label>
-            <div className='red-text'>{touched && error}</div>
+            {_renderError(touched, error)}
         </div>
     );
-
 };
