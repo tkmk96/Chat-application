2bf61252ab8143f976ee55c5d9b2d0237f4e0fca
Lenka Janeckova <lenka.janeckova88@gmail.com>
2017-11-28 13:39:41 +0100

    Message icons and delete message

--- src/actions/messageActions.js
+++ src/actions/messageActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {API_URL, APP_ID, API_CHANNEL} from '../constants/api';
 import {SET_ACTIVE_CHANNEL, CREATE_MESSAGE, FETCH_MESSAGES} from '../constants/actionTypes';
 import {uuid} from '../utils/uuidGenerator';
+import {setActiveChannel} from './channelActions';
 
 export const createMessage = (text) => {
 //POST /api/app/{appId}/channel/{channelId}/message
@@ -28,5 +29,22 @@ export const createMessage = (text) => {
             type: CREATE_MESSAGE,
             payload: {activeChannel, message: res.data}
         })
+    }
+};
+
+export const deleteMessage = (id) => {
+    return async (dispatch, getState) => {
+        const token = getState().authToken;
+        const activeChannel = getState().activeChannel;
+
+        const res = await axios({
+            method: 'delete',
+            url: `${API_URL}/app/${APP_ID}/channel/${activeChannel.id}/message/${id}`,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `bearer ${token}`
+            }
+        });
+        dispatch(setActiveChannel(activeChannel.id));
     }
 };
--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -1,18 +1,49 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {deleteMessage} from '../../actions/messageActions';
 
 class Message extends Component {
     render() {
         let {message, user} = this.props;
         return (
-            <div>
-                <div className={`chip message ${this.props.myMessage}`}>
-                    <img src={user.avatarUrl} title={user.name}/>
-                    <p className='messageText'>{message.value}</p>
-                </div>
+            <div className='fixed-action-btn horizontal click-to-toggle messageWrapper'>
+                <a>
+                    <div className={`chip message ${this.props.myMessage}`}>
+                        <img src={user.avatarUrl} title={user.name}/>
+                        <p className='messageText'>{message.value}</p>
+                    </div>
+                </a>
+                <ul>
+                    {this.props.myMessage && this._renderDeleteButton()}
+                    {this.props.myMessage && this._renderEditButton()}
+                    <li>
+                        <a className='btn-floating blue darken-2' title='Delete'>
+                            <i className='material-icons'>thumb_up</i>
+                        </a>
+                    </li>
+                    <li><a className='btn-floating blue darken-2' title='Delete'><i className='material-icons'>thumb_down</i></a></li>
+
+                </ul>
             </div>
 
         );
     }
+
+    _renderDeleteButton() {
+        return (<li>
+            <a className='btn-floating red darken-2' title='Delete' onClick={() => this._deleteMessage()}>
+                <i className='material-icons'>delete</i>
+            </a>
+        </li>);
+    }
+
+    _renderEditButton() {
+        return <li><a className='btn-floating yellow darken-2' title='Delete'><i className='material-icons'>edit</i></a></li>;
+    }
+
+    _deleteMessage() {
+        this.props.deleteMessage(this.props.message.id);
+    }
 }
 
-export default Message;
+export default connect(null, {deleteMessage})(Message);
