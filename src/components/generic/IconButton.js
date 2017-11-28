a57e3b3e83f10040a731cdd7d6ea7348cb39029b
tkmk96 <tkmk96@gmail.com>
2017-11-28 14:42:58 +0100

    Added generic component for materialize Icons

---
+++ src/components/generic/IconButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default ({title, className, style, iconName, onClick}) => {
+    return (
+        <a className={`btn-floating ${className}`}
+            title={title}
+            style={style}
+            onClick={() => onClick()}
+        >
+            <i className='material-icons'>{iconName}</i>
+        </a>
+    );
+};
