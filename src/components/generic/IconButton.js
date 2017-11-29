3cf9c8c4556e7a2d8ee5a42d9cd4784bd71fae37
tkmk96 <tkmk96@gmail.com>
2017-11-30 00:19:03 +0100

    Fixed usage of IconButton without prop onClick

--- src/components/generic/IconButton.js
+++ src/components/generic/IconButton.js
@@ -5,7 +5,7 @@ export default ({title, className, style, iconName, onClick}) => {
         <a className={`btn-floating ${className}`}
             title={title}
             style={style}
-            onClick={() => onClick()}
+            onClick={() => onClick && onClick()}
         >
             <i className='material-icons'>{iconName}</i>
         </a>
